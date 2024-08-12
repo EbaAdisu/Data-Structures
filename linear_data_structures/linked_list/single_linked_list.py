24b01c2600879c05fe6c0ced1f950e7ef90fab8a
Eba Adisu <ebaadisu2@gmail.com>
2024-08-12 21:14:19 +0300

    Add double linked list and single linked list classes

---
+++ linear_data_structures/linked_list/single_linked_list.py
@@ -0,0 +1,70 @@
+from ..list_node import ListNode
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def insert_at_beginning(self, value: any) -> None:
+        pass
+
+    def instert_at_end(self, value: any) -> None:
+        pass
+
+    def insert_at_position(self, position: int, value: any) -> ListNode:
+        pass
+
+    def get_head(self) -> ListNode:
+        pass
+
+    def get_tail(self) -> ListNode:
+        pass
+
+    def get_at_position(self, position: int) -> ListNode:
+        pass
+
+    def update_at_position(self, position, value) -> None:
+        pass
+
+    def delete_at_beginning(self) -> None:
+        pass
+
+    def delete_at_end(self) -> None:
+        pass
+
+    def delete_at_position(self, position) -> None:
+        pass
+
+    def find(self, value) -> ListNode:
+        pass
+
+    def size(self) -> int:
+        pass
+
+    def is_empty(self) -> bool:
+        pass
+
+    # Additional methods
+
+    def reverse(self) -> None:
+        pass
+
+    def print_list(self) -> None:
+        pass
+
+    def to_list(self) -> list:
+        pass
+
+    def from_list(self, values) -> None:
+        pass
+
+    def has_cycle(self) -> bool:
+        pass
+
+    def remove_duplicate(self) -> None:
+        pass
+
+    def merge_with(self, other) -> None:
+        pass
